0f2a8039b41c7241c69385404644ef08a2c4cce9
Bret Petersen <bretpetersen82@gmail.com>
2021-10-01 12:01:38 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Template START //
+
+// selectors
+var firstChildUl = document.getElementById("first-child-ul");
+
+// global variables
+var myName = "Bret";
+
+// functions
+function doSomethign() {}
+
+// Template END //
+
+// - EXAMPLES - //
+
+// Declaring a variable - this is an example
+var studentName;
+
+// Uses assignment operator(=) to assign a value
+var studentName = "Abdul";
+
+// To re-assign a variable, use only the variable's name
+studentName = "Tonya";
+
+// To access a value stored in a variable, use the variable's name
+console.log(studentName);
+
+//To combine the message with a variable value use the concatenation operator(+)
+//Logs "My name is "
+console.log("My name is ");
+
+// Logs "My name is Tonya"
+console.log("My name is " + studentName);
