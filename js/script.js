96ac27ff427c5ec652498ff2b848ffa9ae5c235f
Bret Petersen <bretpetersen82@gmail.com>
2021-10-02 13:18:10 -0500

    Merge attempt #2

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ fetch(
   .then(function (data) {
     console.log(data);
     //Loop over the data to generate a table, each table row will have a link to the repo url
-    for (var i = 0; i < data.length; i++) {
+    // for (var i = 0; i < data.length; i++) {
       // Do shit to each element in array
     }
   });
